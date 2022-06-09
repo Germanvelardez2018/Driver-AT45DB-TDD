e25ed004a395a0e71c6df3f71994bbdee8351add
root <root@Germanvelardez.peron>
2022-06-09 10:38:21 -0300

    probando ceedling en un proyecto generado con STM32CUBEMX

---
+++ test/test/test_drivers.c
@@ -0,0 +1,21 @@
+#include "unity.h"
+#include "mock_stm32f4xx_hal.h"
+
+#include "mock_stm32f4xx_hal_gpio.h"
+
+
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+
+
+
+void test_driver_init(void){
+    TEST_FAIL_MESSAGE("FAIL");
+}
